f60df006b18f92d748753bc797586fe6fff24f09
iKrusz <bartosz@247studio.co>
2026-02-23 11:45:09 +0100

    Add main structure and heading to Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import data from '@/app/sampleData.json'; // Use this sample data for the task.
 
 export default function Home() {
-  return <main className="min-h-screen items-center justify-center bg-black"></main>;
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black">
+      <h1 className="font-secondary text-[2.5rem] text-white uppercase xl:text-[5rem]">
+        Powodzenia!
+      </h1>
+    </main>
+  );
 }
